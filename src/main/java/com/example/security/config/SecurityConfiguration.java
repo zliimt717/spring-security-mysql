986082cac62dc474a332d1c79bb065fe7734c7cb
xin.gu <xin.gu@accenture.com>
2022-10-21 21:18:54 +0200

    only delete some import

--- src/main/java/com/example/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/security/config/SecurityConfiguration.java
@@ -1,16 +1,11 @@
 package com.example.security.config;
 
-import com.example.security.service.AppUserService;
-import org.hibernate.bytecode.enhance.internal.tracker.NoopCollectionTracker;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
