e468fc4efa49ddb0f6877d929ee3aabcd91620bd
xin.gu <xin.gu@accenture.com>
2022-10-24 13:40:14 +0200

    add code to generate token

--- src/main/java/com/example/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/security/config/SecurityConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfiguration {
 
     @Bean
@@ -22,6 +23,18 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        return http.build();
     }
 
 
--- src/main/java/com/example/security/controller/AppUserController.java
+++ src/main/java/com/example/security/controller/AppUserController.java
@@ -1,12 +1,40 @@
 package com.example.security.controller;
 
+import com.example.security.entity.AuthRequest;
+import com.example.security.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppUserController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
   @GetMapping(path = "/")
   public String welCome(){
       return "Welcome to SpringSecurityApplication";
+  }
+
+  @PostMapping(path = "/authenticate")
+  public String generateToken(@RequestBody AuthRequest authRequest)throws Exception{
+      try{
+          authenticationManager.authenticate(
+                  new UsernamePasswordAuthenticationToken(
+                          authRequest.getUserName(),authRequest.getPassWord())
+          );
+
+      }catch (Exception e){
+          throw new Exception("invalid username/password");
+      }
+      return jwtUtil.generateToken(authRequest.getUserName());
   }
 }
